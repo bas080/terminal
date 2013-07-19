99f72061940c991dfcd0586d48bfb7cd2d104e6b
bas080 <bas080@hotmail.com>
2013-07-19 05:58:31 +0200

    Added mesecons and digilines support, is now diggable and added logout command

--- init.lua
+++ init.lua
@@ -3,9 +3,13 @@ minetest.register_node("terminal:client", {
   tiles = {"terminal_client_side.png", "terminal_client_side.png", "terminal_client_side.png", "terminal_client_side.png", "terminal_client_side.png", "terminal_client_off.png"},
   paramtype = "light",
   paramtype2 = "facedir",
-  groups = {},
+  groups = {dig_immediate=2},
   description="Client",
-  on_punch = function (pos)
+  on_construct = function(pos)
+    local meta = minetest.env:get_meta(pos)
+    meta:set_string("infotext", "")
+  end,
+  on_rightclick = function (pos)
     minetest.sound_play("terminal_on",{pos=pos,gain=0.7,max_hear_distance=32})
     local facing = minetest.env:get_node(pos).param2
     minetest.env:set_node(pos, { name="terminal:client_on", param2=facing })
@@ -23,43 +27,68 @@ minetest.register_node("terminal:client_on", {
   description = "Client",
   on_construct = function(pos)
     local meta = minetest.env:get_meta(pos)
-    meta:set_string("formspec", "field[text;;${text}]")
+    meta:set_string("formspec", "field[text;;${command}]")
+    meta:set_string("channel", "terminal")
+    meta:set_string("command", "")
   end,
   on_receive_fields = function(pos, formname, fields, sender)
-		local meta = minetest.get_meta(pos)
-		fields.text = fields.text or ""
-		meta:set_string("text", fields.text)
-		local terminal_output = terminal_command(fields.text, sender, pos)
-		if (terminal_output == "exit") then
-		  local facing = minetest.env:get_node(pos).param2
-		  minetest.env:set_node(pos, { name="terminal:client", param2=facing })
-		else
-		  meta:set_string("infotext", terminal_output)
-		end
-	end,
+    local meta = minetest.get_meta(pos)
+    meta:set_string("command", fields.text)
+    local terminal_output = terminal_command(fields.text, sender:get_player_name(), pos)
+    if (terminal_output == "exit") then
+      local facing = minetest.env:get_node(pos).param2
+      minetest.env:set_node(pos, { name="terminal:client", param2=facing })
+    else
+      meta:set_string("infotext", terminal_output)
+    end
+  end,
   on_punch = function (pos, node, puncher)
     local meta = minetest.env:get_meta(pos)
-    local command = meta:get_string("text")
-    local terminal_output = terminal_command(command, puncher, pos)
-		meta:set_string("infotext", terminal_output)
+    local command = meta:get_string("command")
+    local terminal_output = terminal_command(command, (puncher:get_player_name()), pos)
+    meta:set_string("infotext", terminal_output)
   end,
-  sounds = default.node_sound_wood_defaults(),
+  mesecons = { effector = {
+    action_on = function (pos, node)
+      local meta = minetest.env:get_meta(pos)
+      local command = meta:get_string("command")
+      local terminal_output = terminal_command(command, "mesecons", pos)
+      meta:set_string("infotext", terminal_output)
+    end,
+  }},
+  digiline = { receptor = {},
+    effector = {
+      action = function(pos, node, channel, msg)
+        local setchan = minetest.env:get_meta(pos):get_string("channel")
+        if setchan ~= channel then return end
+        local meta = minetest.env:get_meta(pos)
+        local command = meta:get_string("command")
+        local terminal_output = terminal_command(command, "digilines", pos)
+        meta:set_string("infotext", terminal_output)
+      end
+    },
+  },
 })
 
 function terminal_command(command, sender, pos)
-  print((sender:get_player_name() or "").." executed \""..command.."\" on client at "..minetest.pos_to_string(pos))
-  if (command == "exit") then
+  print(sender.." executed \""..command.."\" on client at "..minetest.pos_to_string(pos))
+  if (command == "exit" or command == "logout") then
     return "exit"
+  end
+  if (string.find(command, "digilines") ~= nil) then
+    print("BAMAMAMAMAMAMA")
+    return "hahaha"
   end
   local state = os.execute(command.." | tee -i > output")
   local f = io.open("output", "r")
   if f then
     local contents = f:read("*all")
-    if (contents == nil or contents == "") then
-      return "> "..command
+    if (contents == nil or contents == "" or contents == "\n") then
+      return "> "..command.."\n..."
     else
       return "> "..command.."\n"..contents
     end
   end
+  minetest.sound_play("terminal_on",{pos=pos,gain=0.7,max_hear_distance=32})
   return "error"
 end
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Run terminal commands on the hosts computer from miles away.
 
 version
 -------
-0.1
+0.2
 
 contributors
 ------------
@@ -17,10 +17,18 @@ http://forum.minetest.net/viewtopic.php?pid=100710
 
 changelog
 ---------
+0.3
+---
+- uses digilines msg parameter
+- a craft
+- using terminals requires a (keyboard) which can only be obtained threw giveme
+- check if cd(change directory) is possible and remember that path
+
 0.2
 ---
-- check if cd(change directory) is possible and remember that path
-- some sort of security which requires a password/privs/tool(keyboard)
+- trigger on digilines or mesecons signal
+- make diggable when turned off
+- can be turned of by typing logout or exit which was already supported
 
 0.1
 ---
